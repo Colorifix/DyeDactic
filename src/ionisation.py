9c44eba1036e2b17284050f764673fd300aca29a
dkarlov <dkar89@gmail.com>
2025-11-21 14:10:47 +0000

    generation of bikaverin analogues

--- src/ionisation.py
+++ src/ionisation.py
@@ -543,7 +543,7 @@ class Molecule:
         1. Species distributions vs pH
         2. Chemical structures of the corresponding protonated forms
         3. Colour vs pH from single conformations
-        4. Colour vs pH from an ensemble of conformations or vibronic spectra
+        4. Colour vs pH from an ensemble of conformations
         """
         if len(self.species) == 0:
             raise RuntimeError("A self.species is empty: first, generate distribution based on the nature of the colourant")
@@ -559,14 +559,12 @@ class Molecule:
             axs[0].plot(x, self.species[i, :], label=str(i))  # molar fraction vs pH for species
 
         axs[1].bar(x, np.ones(self.num_points), color=self.colours_vs_pH)  # bars marked by computed colour single conformations
-        axs[1].set_title(f'Colour vs pH for {self.name} with gaussian approximation of spectra')
 
         if sampled_spectrum:
             axs[2].bar(x, np.ones(self.num_points), color=self.colours_vs_pH_sampled)  # bars marked by computed colour single conformations
-            axs[2].set_title(f'Colour vs pH for {self.name} based on sampled conformations')
         elif vibronic:
             axs[2].bar(x, np.ones(self.num_points), color=self.colours_vs_pH_vibronic)
-            axs[2].set_title(f'Colour vs pH for {self.name} using vibronic correction')
+
         # draw mols from SMILES
         for i in range(len(self.protonated_species)):
             newax = fig.add_axes([i / len(self.protonated_species),
@@ -596,6 +594,7 @@ class Molecule:
             axs[0].tick_params(bottom=False, labelbottom=False, labelsize=15)
             axs[1].tick_params(labelsize=15)
             axs[2].axis('off')
+
 
         plt.show()
 
